b6518d00efe47cd896ca403689976f505914f60a
surajit77707 <surajitsingh77707@gmail.com>
2025-02-12 22:23:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,7 +95,7 @@ LINK_MODE = is_enabled('LINK_MODE', True)
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or Flase
 # Online Stream and Download
 
 MULTI_CLIENT = False
